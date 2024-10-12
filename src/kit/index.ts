94e8cb7e770ebd230a41bf95c264632c5f6112a2
Luke Nelson <luke@nelson.zone>
2024-10-13 00:13:51 +0100

    Initial idea

---
+++ src/kit/index.ts
@@ -0,0 +1,5 @@
+export { defineNitroPreset, getDefaultNodeVersion } from "./preset";
+export { defineNitroModule } from "./module";
+
+export { writeFile, isDirectory } from "./fs";
+export { prettyPath, resolveNitroPath } from "./path";
