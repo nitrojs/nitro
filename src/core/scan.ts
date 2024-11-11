82911e06bae90b4e8fd8b6b488598c73e8c70b75
John Tanzer <82201261+moshetanzer@users.noreply.github.com>
2024-11-11 14:23:12 +0200

    chore: warn about kebab-case

--- src/core/scan.ts
+++ src/core/scan.ts
@@ -112,6 +112,9 @@ export async function scanServerRoutes(
 
     route = route.replace(/\/index$/, "") || "/";
 
+    if (/[a-z]+-[a-z]+/.test(route)) {
+      console.warn(`Warning: Route "${route}" contains kebab-case parameters which will return 404.`);
+    }
     return {
       handler: file.fullPath,
       lazy: true,
