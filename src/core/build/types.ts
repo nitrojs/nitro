13deac994628e3d1ca6784326041b6cd60540951
Pooya Parsa <pooya@pi0.io>
2025-02-03 16:48:03 +0100

    avoid behavior change from same pr

--- src/core/build/types.ts
+++ src/core/build/types.ts
@@ -49,11 +49,10 @@ export async function writeTypes(nitro: Nitro) {
   if (nitro.unimport) {
     await nitro.unimport.init();
     // TODO: fully resolve utils exported from `#imports`
-    autoImportExports = toExports(
-      await nitro.unimport.getImports(),
-      nitro.options.rootDir,
-      true
-    ).replace(/#internal\/nitro/g, relative(typesDir, runtimeDir));
+    autoImportExports = toExports(await nitro.unimport.getImports()).replace(
+      /#internal\/nitro/g,
+      relative(typesDir, runtimeDir)
+    );
 
     const resolvedImportPathMap = new Map<string, string>();
     const imports = await nitro.unimport
