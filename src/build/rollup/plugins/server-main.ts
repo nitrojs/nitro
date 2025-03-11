650e7c11be4cf7bc5a1ca31ec1c7d77c2409fc6d
Pooya Parsa <pooya@pi0.io>
2025-03-11 15:25:48 +0100

    refactor!: avoid `import.meta` polyfill

---
+++ src/build/rollup/plugins/server-main.ts
@@ -0,0 +1,15 @@
+import type { Nitro } from "nitro/types";
+import type { Plugin } from "rollup";
+
+export function serverMain(nitro: Nitro): Plugin {
+  return {
+    name: "nitro:server-main",
+    renderChunk(code, chunk) {
+      if (chunk.isEntry) {
+        return {
+          code: `globalThis.__nitro_main__ = import.meta.url; ${code}`,
+        };
+      }
+    },
+  };
+}
