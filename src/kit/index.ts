d1bb7c13231b90884178f4a9ae27688c566f5788
Luke Nelson <luke@nelson.zone>
2025-07-17 20:54:36 +0100

    fix: Remove file from merge

--- src/kit/index.ts
+++
@@ -1,5 +0,0 @@
-export { defineNitroPreset, getDefaultNodeVersion } from "./preset";
-export { defineNitroModule } from "./module";
-
-export { writeFile, isDirectory } from "./fs";
-export { prettyPath, resolveNitroPath } from "./path";
