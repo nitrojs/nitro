4b262439ff42f1f13987d554ab2a289bf7524e44
Pooya Parsa <pooya@pi0.io>
2025-08-19 21:10:26 +0200

    fix(vercel): workaround for isr route rule with `passQuery: true`

--- src/presets/vercel/runtime/vercel.ts
+++ src/presets/vercel/runtime/vercel.ts
@@ -2,18 +2,26 @@ import "#nitro-internal-pollyfills";
 import { useNitroApp } from "nitropack/runtime";
 
 import { type NodeListener, toNodeListener } from "h3";
-import { parseQuery } from "ufo";
+import { parseQuery, withQuery } from "ufo";
 
 const nitroApp = useNitroApp();
 
 const handler = toNodeListener(nitroApp.h3App);
 
 const listener: NodeListener = function (req, res) {
-  const query = req.headers["x-now-route-matches"] as string;
-  if (query) {
-    const { url } = parseQuery(query);
+  const isrRoute = req.headers["x-now-route-matches"] as string;
+  if (isrRoute) {
+    const { url } = parseQuery(isrRoute);
     if (url) {
       req.url = url as string;
+    }
+  } else if (req.url?.startsWith("/__fallback")) {
+    // Workaround for ISR functions with passQuery: true
+    // /__fallback--api-weather?url=%2Fapi%2Fweather%2Famsterdam&units=123"
+    const urlQueryIndex = req.url.indexOf("?url=");
+    if (urlQueryIndex !== -1) {
+      const { url, ...params } = parseQuery(req.url.slice(urlQueryIndex));
+      req.url = withQuery((url as string) || "/", params);
     }
   }
   return handler(req, res);
