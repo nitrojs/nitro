468d79df2eef48e4ae589b4880653cb5ed401609
Pooya Parsa <pooya@pi0.io>
2025-10-23 13:07:57 +0200

    only rely on alt workaround

--- src/rollup/config.ts
+++ src/rollup/config.ts
@@ -246,8 +246,7 @@ export const getRollupConfig = (nitro: Nitro): RollupConfig => {
   rollupConfig.plugins.push(
     replace({
       preventAssignment: true,
-      // https://github.com/rollup/plugins/tree/master/packages/replace#delimiters
-      replaceDelimiters: [String.raw`\b`, String.raw`(?![\w.$])`],
+      // replaceDelimiters: [String.raw`\b`, String.raw`(?![\w.$])`],
       values: {
         "typeof window": '"undefined"',
         "typeof window$1": '"undefined"',
