d9ee35f55c5cb8a11d2f6750c69ba3aee4b91ca4
Pooya Parsa <pyapar@gmail.com>
2025-10-23 13:32:21 +0200

    fix(build): avoid replacing `window$1` (#3672)
    Co-authored-by: Vida Xie <vida_2020@163.com>

--- src/rollup/config.ts
+++ src/rollup/config.ts
@@ -246,8 +246,10 @@ export const getRollupConfig = (nitro: Nitro): RollupConfig => {
   rollupConfig.plugins.push(
     replace({
       preventAssignment: true,
+      // delimiters: [String.raw`\b`, String.raw`(?![\w.$])`], // Enabled in nitro v3
       values: {
         "typeof window": '"undefined"',
+        "typeof window$1": "typeof window$1", // vueuse/vueuse#5114
         _import_meta_url_: "import.meta.url",
         "globalThis.process.": "process.",
         "process.env.RUNTIME_CONFIG": () =>
