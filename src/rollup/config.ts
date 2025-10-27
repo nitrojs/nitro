50c61c89b3e891fabc5eb1b536d2c6fd8e4788ed
James Garbutt <43081j@users.noreply.github.com>
2025-10-27 22:26:47 +0000

    fix: set explicit delimiters to allow $ in replacements (#3689)

--- src/rollup/config.ts
+++ src/rollup/config.ts
@@ -246,10 +246,9 @@ export const getRollupConfig = (nitro: Nitro): RollupConfig => {
   rollupConfig.plugins.push(
     replace({
       preventAssignment: true,
-      // delimiters: [String.raw`\b`, String.raw`(?![\w.$])`], // Enabled in nitro v3
+      delimiters: [String.raw`\b`, String.raw`\b(?![\.$])`],
       values: {
         "typeof window": '"undefined"',
-        "typeof window$1": "typeof window$1", // vueuse/vueuse#5114
         _import_meta_url_: "import.meta.url",
         "globalThis.process.": "process.",
         "process.env.RUNTIME_CONFIG": () =>
