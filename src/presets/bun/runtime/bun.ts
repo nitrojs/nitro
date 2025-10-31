1ee0583d88ac299ad108899a2d4160df6e2f67b4
Mr. <gomexxxx71@gmail.com>
2025-10-31 12:32:18 +0600

    chore: update

--- src/presets/bun/runtime/bun.ts
+++ src/presets/bun/runtime/bun.ts
@@ -10,7 +10,7 @@ const cert = process.env.NITRO_SSL_CERT;
 const key = process.env.NITRO_SSL_KEY;
 
 if (port < 0 || port > 65535) {
-  throw new Error("Port number range is must between 0 to 65535");
+  throw new Error("Port number range is between 0 to 65535");
 }
 
 // const socketPath = process.env.NITRO_UNIX_SOCKET; // TODO
