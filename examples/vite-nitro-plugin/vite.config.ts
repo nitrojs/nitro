3d51f2e2e5fa26e588a06811994b2d005e6d3eca
Pooya Parsa <pooya@pi0.io>
2025-10-31 20:08:22 +0100

    use normal order

--- examples/vite-nitro-plugin/vite.config.ts
+++ examples/vite-nitro-plugin/vite.config.ts
@@ -3,7 +3,7 @@ import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
-    // nitro(),
+    nitro(),
     {
       name: "my-nitro-plugin",
       nitro: {
@@ -14,6 +14,5 @@ export default defineConfig({
         },
       },
     },
-    nitro(),
   ],
 });
