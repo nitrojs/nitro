653e4e6ee4ea1bfbcbdf10dadd3e870e4e44b15d
Pooya Parsa <pooya@pi0.io>
2025-11-03 13:15:41 +0100

    feat: `routes` config

--- src/types/config.ts
+++ src/types/config.ts
@@ -183,12 +183,16 @@ export interface NitroOptions extends PresetOptions {
   // Routing
   baseURL: string;
   apiBaseURL: string;
-  serverEntry: { handler?: string; format?: EventHandlerFormat } | undefined;
+
+  routes: Record<string, Omit<NitroEventHandler, "route" | "middleware">>;
   handlers: NitroEventHandler[];
-  routeRules: { [path: string]: NitroRouteRules };
   devHandlers: NitroDevEventHandler[];
+
+  routeRules: { [path: string]: NitroRouteRules };
+
   errorHandler: string | string[];
   devErrorHandler: NitroErrorHandler;
+
   prerender: {
     /**
      * Prerender HTML routes within subfolders (`/test` would produce `/test/index.html`)
