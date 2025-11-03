9c403e1b1db72a4e51581509857d26f4483e6ab4
Pooya Parsa <pooya@pi0.io>
2025-11-03 09:52:33 +0100

    refactor!: register `server.ts` as `/**` route

--- test/minimal/server.ts
+++ test/minimal/server.ts
@@ -1,5 +1,6 @@
 export default {
   fetch(request: Request) {
-    return new Response("ok");
+    // return new Response("ok");
+    return new Response("404 Not Found", { status: 404 });
   },
 };
