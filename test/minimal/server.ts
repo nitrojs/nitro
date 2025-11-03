94eddc8e9f89ec9cdb92d08b79cdedf12f5b8947
Pooya Parsa <pooya@pi0.io>
2025-11-03 10:01:55 +0100

    revert minimal

--- test/minimal/server.ts
+++ test/minimal/server.ts
@@ -1,6 +1,5 @@
 export default {
-  fetch(request: Request) {
-    // return new Response("ok");
-    return new Response("404 Not Found", { status: 404 });
+  fetch(_req: Request) {
+    return new Response("ok");
   },
 };
