2c4d41a64c6b91430310adc1ff0b77d63ab12b29
Pooya Parsa <pooya@pi0.io>
2025-11-04 11:36:51 +0100

    feat: add `defineConfig` and type utils from `"nitro"` import

---
+++ src/runtime/nitro.ts
@@ -0,0 +1,18 @@
+// Config
+import type { NitroConfig } from "nitro/types";
+
+export function defineNitroConfig(
+  config: Omit<NitroConfig, "rootDir">
+): Omit<NitroConfig, "rootDir"> {
+  return config;
+}
+
+export { defineNitroConfig as defineConfig };
+
+// Type (only) helpers
+export { defineNitroPlugin } from "./internal/plugin.ts";
+export { defineRouteMeta } from "./internal/meta.ts";
+export { defineNitroErrorHandler } from "./internal/error/utils.ts";
+
+// Context
+export { useRequest } from "./internal/context.ts";
