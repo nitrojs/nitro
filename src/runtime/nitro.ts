1cebcc5de8f89b428b12545b77a5ebe4ba65bf6a
Pooya Parsa <pooya@pi0.io>
2025-11-04 12:26:48 +0100

    update

--- src/runtime/nitro.ts
+++ src/runtime/nitro.ts
@@ -1,15 +1,12 @@
 // Config
 import type { NitroConfig } from "nitro/types";
 
-export function defineNitroConfig(
+export function defineConfig(
   config: Omit<NitroConfig, "rootDir">
 ): Omit<NitroConfig, "rootDir"> {
   return config;
 }
 
-export { defineNitroConfig as defineConfig };
-
 // Type (only) helpers
-export { defineNitroPlugin } from "./internal/plugin.ts";
+export { defineNitroPlugin as definePlugin } from "./internal/plugin.ts";
 export { defineRouteMeta } from "./internal/meta.ts";
-export { defineNitroErrorHandler } from "./internal/error/utils.ts";
