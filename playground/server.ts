5689e37ab6363f0070836ea0e21834e9567923a7
Pooya Parsa <pooya@pi0.io>
2025-11-07 00:51:55 +0100

    update

--- playground/server.ts
+++ playground/server.ts
@@ -1,5 +1,7 @@
+import conf from "~/nitro.config";
 export default {
   fetch(req: Request) {
+    console.log("Nitro config:", conf);
     return new Response("Hello from Nitro playground!");
   },
 };
--- src/config/loader.ts
+++ src/config/loader.ts
@@ -20,7 +20,7 @@ import { resolveDatabaseOptions } from "./resolvers/database.ts";
 import { resolveExportConditionsOptions } from "./resolvers/export-conditions.ts";
 import { resolveImportsOptions } from "./resolvers/imports.ts";
 import { resolveOpenAPIOptions } from "./resolvers/open-api.ts";
-import { resolveTypescript } from "./resolvers/typescript.ts";
+import { resolveTsconfig } from "./resolvers/tsconfig.ts";
 import { resolvePathOptions } from "./resolvers/paths.ts";
 import { resolveRouteRulesOptions } from "./resolvers/route-rules.ts";
 import { resolveRuntimeConfigOptions } from "./resolvers/runtime-config.ts";
@@ -32,7 +32,7 @@ import { resolveBuilder } from "./resolvers/builder.ts";
 
 const configResolvers = [
   resolveCompatibilityOptions,
-  resolveTypescript,
+  resolveTsconfig,
   resolvePathOptions,
   resolveImportsOptions,
   resolveRouteRulesOptions,
