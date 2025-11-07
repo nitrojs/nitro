d17a84db00309e1aa8b8379f278720b0262de923
Pooya Parsa <pooya@pi0.io>
2025-11-07 11:02:44 +0100

    add `import-alias` example

--- playground/server.ts
+++ playground/server.ts
@@ -1,7 +1,5 @@
-import conf from "~/nitro.config";
 export default {
   fetch(req: Request) {
-    console.log("Nitro config:", conf);
     return new Response("Hello from Nitro playground!");
   },
 };
