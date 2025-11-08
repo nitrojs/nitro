05d7d32e5e445cdb05c701535f0eb74dc855c436
Pooya Parsa <pooya@pi0.io>
2025-11-08 18:26:43 +0100

    fix(vite): reuse `noExternal` list for nitro env

--- src/build/plugins.ts
+++ src/build/plugins.ts
@@ -116,27 +116,7 @@ export function baseBuildPlugins(nitro: Nitro, base: BaseBuildConfig) {
           outDir: nitro.options.output.serverDir,
           moduleDirectories: nitro.options.nodeModulesDirs,
           external: nitro.options.nodeModulesDirs,
-          inline: [
-            "#",
-            "~",
-            "@/",
-            "~~",
-            "@@/",
-            "virtual:",
-            "nitro",
-            pkgDir,
-            nitro.options.serverDir,
-            dirname(nitro.options.entry),
-            ...(nitro.options.experimental.wasm
-              ? [(id: string) => id?.endsWith(".wasm")]
-              : []),
-            ...nitro.options.handlers
-              .map((m) => m.handler)
-              .filter((i) => typeof i === "string"),
-            ...(nitro.options.dev || nitro.options.preset === "nitro-prerender"
-              ? []
-              : runtimeDependencies),
-          ].filter(Boolean) as string[],
+          inline: [...base.noExternal],
           traceOptions: {
             base: "/",
             processCwd: nitro.options.rootDir,
