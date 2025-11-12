ab687be6f59ceb33dffb792fce7e8e24656e634a
Pooya Parsa <pooya@pi0.io>
2025-11-12 14:20:33 +0100

    enable: enable native wasm support by default

--- src/build/config.ts
+++ src/build/config.ts
@@ -45,9 +45,9 @@ export function baseBuildConfig(nitro: Nitro) {
     nitro.options.serverDir,
     nitro.options.buildDir,
     dirname(nitro.options.entry),
-    ...(nitro.options.experimental.wasm
-      ? [(id: string) => id?.endsWith(".wasm")]
-      : []),
+    ...(nitro.options.wasm === false
+      ? []
+      : [(id: string) => id?.endsWith(".wasm")]),
     ...nitro.options.handlers
       .map((m) => m.handler)
       .filter((i) => typeof i === "string"),
