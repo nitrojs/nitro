2f1b2c95f0b917cc7d807561ecbcb49ece9a61ce
Pooya Parsa <pooya@pi0.io>
2025-11-13 09:27:12 +0100

    noExternal type fix

--- src/build/config.ts
+++ src/build/config.ts
@@ -33,7 +33,7 @@ export function baseBuildConfig(nitro: Nitro) {
     ...nitro.options.replace,
   };
 
-  const noExternal = [
+  const noExternal: (string | RegExp | ((id: string) => boolean))[] = [
     "#",
     "~",
     "@/",
