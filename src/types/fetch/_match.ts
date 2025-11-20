57043c38c3985812b7d417b85142a5b1abfeb01e
KazariEX <zinkawaii@qq.com>
2025-11-21 01:11:40 +0800

    chore: lint

--- src/types/fetch/_match.ts
+++ src/types/fetch/_match.ts
@@ -91,8 +91,8 @@ export type MatchedRoutes<
 > = Route extends "/"
   ? keyof InternalApi // root middleware
   : Extract<Matches, { exact: true }> extends never
-    ? // @ts-ignore
-      | Extract<
+    ?
+        | Extract<
             Exclude<Matches, { score: never }>,
             { score: MaxTuple<Matches["score"]> }
           >["key"]
