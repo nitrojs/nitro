9086728f301d07d6059fc976c76a233f8f295656
Pooya Parsa <pooya@pi0.io>
2025-12-11 17:57:46 +0100

    add missing server-assets export

--- src/runtime/virtual/server-assets.ts
+++ src/runtime/virtual/server-assets.ts
@@ -1,5 +1,9 @@
 import "./_runtime_warn.ts";
+import { createStorage } from "unstorage";
+
 import type { AssetMeta } from "nitro/types";
+
+export const assets = createStorage();
 
 export function readAsset<T = any>(_id: string): Promise<T> {
   return Promise.resolve({} as T);
