e1ee65e4a50b208019a657fbac13b1343704a491
Pooya Parsa <pooya@pi0.io>
2025-12-12 13:34:01 +0100

    update tss to tailwind plugin

--- examples/vite-ssr-tss-react/vite.config.mjs
+++ examples/vite-ssr-tss-react/vite.config.mjs
@@ -3,17 +3,16 @@ import { nitro } from "nitro/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
     tanstackStart(),
     viteReact(),
+    tailwindcss(),
     nitro(),
   ],
-  experimental: {
-    enableNativePlugin: false,
-  },
   environments: {
     ssr: { build: { rollupOptions: { input: "./server.ts" } } },
   },
