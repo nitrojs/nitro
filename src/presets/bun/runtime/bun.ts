da19880150530a259e4cb75f92124d6799f8802b
Pooya Parsa <pyapar@gmail.com>
2025-12-16 11:35:48 +0100

    Update bun.ts

--- src/presets/bun/runtime/bun.ts
+++ src/presets/bun/runtime/bun.ts
@@ -12,9 +12,7 @@ const ws = import.meta._websocket
 
 // @ts-expect-error
 const server = Bun.serve({
-  idleTimeout: process.env.BUN_IDLE_TIMEOUT
-    ? Number.parseInt(process.env.BUN_IDLE_TIMEOUT)
-    : undefined,
+  idleTimeout: Number.parseInt(process.env.NITRO_BUN_IDLE_TIMEOUT) || undefined,
   port: process.env.NITRO_PORT || process.env.PORT || 3000,
   websocket: import.meta._websocket ? ws!.websocket : (undefined as any),
   async fetch(req: Request, server: any) {
