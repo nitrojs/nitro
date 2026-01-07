f059e4ba7ea0217b63fec7b8bc2e4a92a5b11b89
Typed SIGTERM <typed.sigterm@gmail.com>
2026-01-07 14:32:30 +0000

    fix: avoid long caching dev assets

--- src/core/dev-server/server.ts
+++ src/core/dev-server/server.ts
@@ -247,7 +247,8 @@ class DevServer {
               );
               if (rules.headers) {
                 for (const [k, v] of Object.entries(rules.headers)) {
-                  res.appendHeader(k, v);
+                  if (k !== 'cache-control') // avoid long caching dev assets
+                    res.appendHeader(k, v);
                 }
               }
             },
