4cfa31cab0a41d77fa43c6bc20fab67fc1a74ff9
Pooya Parsa <pooya@pi0.io>
2026-02-12 15:30:14 +0100

    update

--- src/config/resolvers/export-conditions.ts
+++ src/config/resolvers/export-conditions.ts
@@ -9,35 +9,28 @@ export async function resolveExportConditionsOptions(options: NitroOptions) {
 }
 
 function _resolveExportConditions(
-  conditions: string[],
+  userConditions: string[],
   opts: { dev: boolean; node: boolean; wasm?: boolean }
 ) {
-  const negated = new Set(conditions.filter((c) => c.startsWith("!")).map((c) => c.slice(1)));
+  const conditions: string[] = [...userConditions.filter((c) => !c.startsWith("!"))];
 
-  const resolvedConditions: string[] = [];
+  conditions.push(opts.dev ? "development" : "production");
 
-  // 1. Add dev or production
-  resolvedConditions.push(opts.dev ? "development" : "production");
-
-  // 2. Add user specified conditions
-  resolvedConditions.push(...conditions.filter((c) => !c.startsWith("!")));
-
-  // 3. Add unwasm conditions
   if (opts.wasm) {
-    resolvedConditions.push("wasm", "unwasm");
+    conditions.push("wasm", "unwasm");
   }
 
-  // 4. Add default conditions
-  // "module" is NOT A STANDARD CONDITION but widely used in the ecosystem adding helps with compatibility
-  resolvedConditions.push("module");
+  if (opts.node) {
+    conditions.push("node");
+  }
 
-  // 5. Auto detect bun and deno (builder)
   if ("Bun" in globalThis) {
-    resolvedConditions.push("bun");
+    conditions.push("bun");
   } else if ("Deno" in globalThis) {
-    resolvedConditions.push("deno");
+    conditions.push("deno");
   }
 
-  // 6. Dedup and remove negated conditions
-  return [...new Set(resolvedConditions)].filter((c) => !negated.has(c));
+  const negated = new Set(userConditions.filter((c) => c.startsWith("!")).map((c) => c.slice(1)));
+
+  return [...new Set(conditions)].filter((c) => !negated.has(c));
 }
