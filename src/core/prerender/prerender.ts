81f7680077e4965ca861a3b11c6cd0c4168c072b
Pooya Parsa <pooya@pi0.io>
2026-02-18 00:49:18 +0100

    fix(prerender): skip writing routes with `..` or outside of public dir

--- src/core/prerender/prerender.ts
+++ src/core/prerender/prerender.ts
@@ -155,8 +155,8 @@ export async function prerender(nitro: Nitro) {
   };
 
   const canWriteToDisk = (route: PrerenderRoute) => {
-    // Cannot write routes with query
-    if (route.route.includes("?")) {
+    // Cannot write routes with query or containing ..
+    if (route.route.includes("?") || route.route.includes("..")) {
       return false;
     }
 
@@ -296,8 +296,10 @@ export async function prerender(nitro: Nitro) {
     // Write to the disk
     if (canWriteToDisk(_route)) {
       const filePath = join(nitro.options.output.publicDir, _route.fileName);
-      await writeFile(filePath, dataBuff);
-      nitro._prerenderedRoutes!.push(_route);
+      if (filePath.startsWith(nitro.options.output.publicDir)) {
+        await writeFile(filePath, dataBuff);
+        nitro._prerenderedRoutes!.push(_route);
+      }
     } else {
       _route.skip = true;
     }
