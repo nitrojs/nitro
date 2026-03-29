da105de751aafd084a1589a7669d84e7ae2ff2c0
Teages <Teages@teages.xyz>
2026-03-30 07:15:13 +0800

    chore: remove unused rolldownConfig from nitro configuration

--- examples/graphql-pothos/nitro.config.ts
+++ examples/graphql-pothos/nitro.config.ts
@@ -2,12 +2,4 @@ import { defineConfig } from "nitro";
 
 export default defineConfig({
   serverDir: "./server",
-
-  rolldownConfig: {
-    output: {},
-    treeshake: {
-      // https://github.com/nitrojs/nitro/issues/4085
-      moduleSideEffects: true,
-    },
-  },
 });
