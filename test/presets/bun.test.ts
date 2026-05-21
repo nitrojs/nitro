f643414591a0d7dde7f1023b680ec1cefde800a5
harshagarwalnyu <harshagarwal48756@gmail.com>
2026-05-20 21:14:04 -0400

    fix(bun): fix typecheck errors and expand prototype assertion in test

--- test/presets/bun.test.ts
+++ test/presets/bun.test.ts
@@ -8,27 +8,32 @@ const hasBun = execaCommandSync("bun --version", { stdio: "ignore", reject: fals
 
 describe.runIf(hasBun)("nitro:preset:bun", async () => {
   const ctx = await setupTest("bun");
-  testNitro(ctx, async () => {
-    const port = await getRandomPort();
-    process.env.PORT = String(port);
-    execa("bun", [resolve(ctx.outDir, "server/index.mjs")], {
-      stdio: "inherit",
-    });
-    ctx.server = {
-      url: `http://127.0.0.1:${port}`,
-      close: () => {
-        // p.kill()
-      },
-    } as any;
-    await waitForPort(port);
-    return async ({ url, ...opts }) => {
-      const res = await ctx.fetch(url, opts);
-      return res;
-    };
-  }, (ctx, callHandler) => {
-    it("bun: ReadableStream polyfill works", async () => {
-      const { data } = await callHandler({ url: "/bun-direct-stream" });
-      expect(data.isStream).toBe(true);
-    });
-  });
+  testNitro(
+    ctx,
+    async () => {
+      const port = await getRandomPort();
+      process.env.PORT = String(port);
+      execa("bun", [resolve(ctx.outDir, "server/index.mjs")], {
+        stdio: "inherit",
+      });
+      ctx.server = {
+        url: `http://127.0.0.1:${port}`,
+        close: () => {
+          // p.kill()
+        },
+      } as any;
+      await waitForPort(port);
+      return async ({ url, ...opts }) => {
+        const res = await ctx.fetch(url, opts);
+        return res;
+      };
+    },
+    (ctx, callHandler) => {
+      it("bun: ReadableStream polyfill works", async () => {
+        const { data } = await callHandler({ url: "/bun-direct-stream" });
+        expect(data.isStream).toBe(true);
+        expect(data.hasCorrectPrototype).toBe(true);
+      });
+    }
+  );
 });
