92a85ef4809b49d38cf57f28be9904f3e6150765
Pooya Parsa <pooya@pi0.io>
2026-09-08 00:10:23 +0000

    fix(openapi): adapt isDepInstalled call to new options signature

--- src/build/plugins/_route-response-schema.ts
+++ src/build/plugins/_route-response-schema.ts
@@ -44,7 +44,7 @@ async function createTypeScriptEngine(
   nitro: Nitro,
   files: string[]
 ): Promise<TypeScriptEngine | undefined> {
-  if (!isDepInstalled("typescript", nitro.options.rootDir)) {
+  if (!isDepInstalled("typescript", { dir: nitro.options.rootDir })) {
     warnMissingTypeScript(nitro);
     return;
   }
